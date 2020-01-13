97707be5a7f5eeada87b6d1ffde45017abffa266
Christoph Wagner <github@pandawhisperer.net>
2020-01-13 01:03:50 -0800

    Replace default content on index page

---
+++ components/Items.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-list-item-group>
+    <v-list-item v-for="item in items" :key="item.id" two-line>
+      <v-list-item-content>
+        <v-list-item-title>
+          {{ item.title }}
+        </v-list-item-title>
+        <v-list-item-subtitle>
+          {{ new Date(item.pubDate).toDateString() }}
+        </v-list-item-subtitle>
+      </v-list-item-content>
+    </v-list-item>
+  </v-list-item-group>
+</template>
+
+<script>
+export default {
+  computed: {
+    items() {
+      return this.$store.state.items
+    }
+  },
+}
+</script>
