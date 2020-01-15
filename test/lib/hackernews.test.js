41bfaec3d263dad1faa8c5918d6a17be4e8bde00
Christoph Wagner <github@pandawhisperer.net>
2020-01-15 15:47:58 -0800

    Add nock for network mocking

--- test/lib/hackernews.test.js
+++ test/lib/hackernews.test.js
@@ -1,23 +1,9 @@
 import axios from 'axios'
+import nock from 'nock'
 import { getStories, getItem } from '~/lib/hackernews'
 
-jest.mock('axios')
-
-beforeEach(() => {
-  // Clear mocks calls
-  axios.mockClear();
-});
-
-describe('getStories', () => {
-  test('type argument must be valid', async () => {
-    expect( getStories('foo') ).rejects.toThrow(/type/)
-  })
-
-  test('defaults to top stories', async () => {
-    axios.get.mockResolvedValue({ data: [], status: 200 })
-    await getStories()
-    expect( axios.get.mock.calls[0][0] ).toMatch(/topstories/)
-  })
+beforeAll(() => {
+  axios.defaults.adapter = require('axios/lib/adapters/http')
 })
 
 describe('getItem', () => {
@@ -25,15 +11,11 @@ describe('getItem', () => {
     expect( getItem() ).rejects.toThrow(/id is required/)
   })
 
-  test('id must be valid', async () => {
-    expect( getItem('foo') ).rejects.toThrow(/id must be numerical/)
-  })
-
   test('with valid id', async () => {
-    const data = {
+    const story = {
+      "id" : 22022466,
       "by" : "danabramov",
       "descendants" : 571,
-      "id" : 22022466,
       "kids" : [ 22022603, 22024416, 22024107 ],
       "score" : 1657,
       "time" : 1578778327,
@@ -41,8 +23,27 @@ describe('getItem', () => {
       "type" : "story",
       "url" : "https://overreacted.io/goodbye-clean-code/"
     }
-    axios.get.mockResolvedValue({ data, status: 200 })
+    const mock = nock('https://hacker-news.firebaseio.com/v0')
+      .get(`/item/${story.id}.json`)
+      .reply(200, story)
 
-    expect( await getItem('22022466') ).toEqual(data)
+    expect( await getItem('22022466') ).toEqual(story)
+    expect( mock.isDone() ).toBe(true)
+  })
+})
+
+describe('getStories', () => {
+  test('type argument must be valid', async () => {
+    expect( getStories('foo') ).rejects.toThrow(/type/)
+  })
+
+  test('defaults to top stories', async () => {
+    const mock = nock('https://hacker-news.firebaseio.com/v0')
+      .get(`/topstories.json`)
+      .reply(200, [])
+
+    await getStories()
+
+    expect( mock.isDone() ).toBe(true)
   })
 })
