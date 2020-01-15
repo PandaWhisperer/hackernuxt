82d252fea6a65f9cbd8ec9671e9295d3e1e61047
Christoph Wagner <github@pandawhisperer.net>
2020-01-15 09:37:06 -0800

    Remove xml2js dependency

--- store/actions.js
+++ store/actions.js
@@ -1,15 +1,5 @@
-import axios from 'axios'
-import { parseStringPromise as parseXml } from 'xml2js'
+import { getStories } from '~/lib/hackernews'
 
-export async function loadItems({ commit }) {
-  const result = await axios.get('https://news.ycombinator.com/rss')
-
-  if (result.status == 200) {
-    const { rss } = await parseXml(result.data, { explicitArray: false })
-    const items = rss.channel.item.map(item => ({
-      id: item.comments.split('=')[1], ...item
-    }))
-
-    commit('setItems', items)
-  }
+export async function loadItems({ commit }, type = 'top') {
+  commit('setItems', await getStories(type))
 }
--- test/store/actions.test.js
+++ test/store/actions.test.js
@@ -5,41 +5,23 @@ jest.mock('axios')
 
 describe('actions', () => {
   test('loadItems', async () => {
-    const data = `
-      <rss version="2.0">
-        <channel>
-          <title>Hacker News</title>
-          <link>https://news.ycombinator.com/</link>
-          <description>Links for the intellectually curious, ranked by readers.</description>
-          <item>
-            <title>Goodbye, Clean Code</title>
-            <link>https://overreacted.io/goodbye-clean-code/</link>
-            <pubDate>Sat, 11 Jan 2020 21:32:07 +0000</pubDate>
-            <comments>https://news.ycombinator.com/item?id=22022466</comments>
-            <description><![CDATA[<a href="https://news.ycombinator.com/item?id=22022466">Comments</a>]]></description>
-          </item>
-          <item>
-            <comments>https://news.ycombinator.com/item?id=12345</comments>
-          </item>
-        </channel>
-      </rss>
-    `
-    const items = [{
-      id: "22022466",
-      title: 'Goodbye, Clean Code',
-      link: 'https://overreacted.io/goodbye-clean-code/',
-      pubDate: 'Sat, 11 Jan 2020 21:32:07 +0000',
-      comments: 'https://news.ycombinator.com/item?id=22022466',
-      description: '<a href="https://news.ycombinator.com/item?id=22022466">Comments</a>'
-    }, {
-      id: "12345",
-      comments: "https://news.ycombinator.com/item?id=12345"
+    const stories = [{
+      "by" : "danabramov",
+      "descendants" : 571,
+      "id" : 22022466,
+      "kids" : [ 22022603, 22024416, 22024107 ],
+      "score" : 1657,
+      "time" : 1578778327,
+      "title" : "Goodbye, Clean Code",
+      "type" : "story",
+      "url" : "https://overreacted.io/goodbye-clean-code/"
     }]
+
     const commit = jest.fn()
 
-    axios.get.mockResolvedValue({ data, status: 200 })
+    axios.get.mockResolvedValue({ data: stories, status: 200 })
 
     await loadItems({ commit })
-    expect(commit.mock.calls[0]).toEqual(['setItems', items])
+    expect( commit ).toHaveBeenCalledWith('setItems', stories)
   })
 })
