1db9dd01be57eff53a6c2e5a71191ff433315126
Christoph Wagner <github@pandawhisperer.net>
2020-01-15 19:11:51 -0800

    Make comment author bold

--- components/Comment.vue
+++ components/Comment.vue
@@ -1,6 +1,8 @@
 <template>
   <v-card>
-    <v-card-subtitle>{{ comment.by }} says:</v-card-subtitle>
+    <v-card-subtitle>
+      <strong>{{ comment.by }} says</strong>
+    </v-card-subtitle>
     <v-card-text v-html="comment.text"/>
   </v-card>
 </template>
