6d85ef2e4d8529b475fec08bcc7eec4c76f9139b
Christoph Wagner <github@pandawhisperer.net>
2020-01-15 19:53:09 -0800

    Preliminary loading button w/ progress

--- components/Comment.vue
+++ components/Comment.vue
@@ -1,17 +1,32 @@
 <template>
-  <v-card tile flat>
+  <v-card tile flat :loading="loading">
     <v-card-subtitle>
       <strong>{{ comment.by }} said</strong>
       {{ $timeAgo(comment.date) }}
     </v-card-subtitle>
     <v-card-text v-html="comment.text"/>
+    <v-card-actions v-if="comment.kids">
+      <v-btn text @click="loadMore">more</v-btn>
+    </v-card-actions>
   </v-card>
 </template>
 
 <script>
 export default {
+  data: () => ({
+    loading: false
+  }),
   props: {
     comment: Object
+  },
+  methods: {
+    loadMore() {
+      this.loading = true
+
+      // this.$store.dispatch('loadComments', this.comment.id)
+
+      setTimeout(() => (this.loading = false), 2000)
+    }
   }
 }
 </script>
