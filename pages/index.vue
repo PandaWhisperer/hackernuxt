2c862acb6a9c041401a7df97dbeb2ad8cf821266
Christoph Wagner <github@pandawhisperer.net>
2020-01-15 16:34:37 -0800

    Rename components

--- pages/index.vue
+++ pages/index.vue
@@ -1,21 +1,21 @@
 <template>
   <v-row>
     <v-col xs="12" sm="5" md="4">
-      <items/>
+      <stories/>
     </v-col>
     <v-col xs="12" sm="7" md="8">
-      <article-view/>
+      <story/>
     </v-col>
   </v-row>
 </template>
 
 <script>
-import Items from '~/components/Items.vue'
-import ArticleView from '~/components/Article.vue'
+import Stories from '~/components/Stories.vue'
+import Story from '~/components/Story.vue'
 
 export default {
   components: {
-    Items, ArticleView
+    Stories, Story
   },
   async fetch({ store, params }) {
     await store.dispatch('loadItems', params.stories)
