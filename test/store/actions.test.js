0c17f8206a8edd73997a7a9362c26f2b40c74023
Christoph Wagner <github@pandawhisperer.net>
2020-01-15 16:00:45 -0800

    Test with nock

--- test/store/actions.test.js
+++ test/store/actions.test.js
@@ -1,14 +1,17 @@
 import axios from 'axios'
+import nock from 'nock'
 import { loadItems } from '~/store/actions'
 
-jest.mock('axios')
+beforeAll(() => {
+  axios.defaults.adapter = require('axios/lib/adapters/http')
+})
 
 describe('actions', () => {
   test('loadItems', async () => {
     const stories = [{
+      "id" : 22022466,
       "by" : "danabramov",
       "descendants" : 571,
-      "id" : 22022466,
       "kids" : [ 22022603, 22024416, 22024107 ],
       "score" : 1657,
       "time" : 1578778327,
@@ -17,11 +20,22 @@ describe('actions', () => {
       "url" : "https://overreacted.io/goodbye-clean-code/"
     }]
 
+    const mock = nock('https://hacker-news.firebaseio.com/v0')
+      .get(`/topstories.json`)
+      .reply(200, stories.map(({id}) => id))
+
+    stories.forEach(story => {
+      mock.get(`/item/${story.id}.json`)
+          .reply(200, story)
+    })
+
     const commit = jest.fn()
-
-    axios.get.mockResolvedValue({ data: stories, status: 200 })
-
     await loadItems({ commit })
-    expect( commit ).toHaveBeenCalledWith('setItems', stories)
+
+    expect( commit ).toHaveBeenCalledWith('setItems',
+      stories.map(story => ({ ...story, date: expect.any(Date) }))
+    )
+
+    mock.done()
   })
 })
