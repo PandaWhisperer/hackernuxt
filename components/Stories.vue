43a430c3c7e7fbb18ef03f9df211d53fd5c1a86b
Christoph Wagner <github@pandawhisperer.net>
2020-01-17 08:12:38 -0800

    Put stories in a card

--- components/Stories.vue
+++ components/Stories.vue
@@ -1,23 +1,25 @@
 <template>
-  <v-list-item-group>
-    <v-list-item
-      v-for="story in stories"
-      :key="story.id"
-      :to="`/stories/${story.id}`"
-      two-line
-    >
-      <v-list-item-content>
-        <v-list-item-title>
-          {{ story.title }}
-        </v-list-item-title>
-        <v-list-item-subtitle>
-          <strong>{{ story.by }}</strong>,
-          {{ $timeAgo(story.date) }},
-          {{ story.descendants }} comments
-        </v-list-item-subtitle>
-      </v-list-item-content>
-    </v-list-item>
-  </v-list-item-group>
+  <v-card>
+    <v-list-item-group>
+      <v-list-item
+        v-for="story in stories"
+        :key="story.id"
+        :to="`/stories/${story.id}`"
+        two-line
+      >
+        <v-list-item-content>
+          <v-list-item-title>
+            {{ story.title }}
+          </v-list-item-title>
+          <v-list-item-subtitle>
+            <strong>{{ story.by }}</strong>,
+            {{ $timeAgo(story.date) }},
+            {{ story.descendants }} comments
+          </v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
+    </v-list-item-group>
+  </v-card>
 </template>
 
 <script>
